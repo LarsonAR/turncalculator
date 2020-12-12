3a149c8e9a8fa43056725cba0c9e63ff32a3bfc7
Ash Larson <larsonannar@gmail.com>
2020-12-11 17:35:14 -0800

    Initial commit

---
+++ vue.js
@@ -0,0 +1,93 @@
+const App = new Vue ({
+    el: "#app",
+    data: {
+        message: "Hello World",
+        dragons: [],
+        monsters: [],
+        newDragon: {
+            name: null,
+            quickness: 0,
+            numAmbush: 0,
+        },
+        newMonster: {
+            name: null,
+            quickness: 0,
+        },
+        turnsCalculated: false,
+        numRounds: null,
+        turns: [],
+    },
+    methods: {
+        addDragon() {
+            this.dragons.push({
+                name: this.newDragon.name,
+                quickness: parseInt(this.newDragon.quickness),
+                ambush: parseInt(this.newDragon.numAmbush),
+                initiative: 0,
+            });
+            this.newDragon.name = null;
+            this.newDragon.quickness = null;
+            this.newDragon.numAmbush = 0;
+        },
+        deleteDragon(dragon) {
+            let index = this.dragons.findIndex(s => s === dragon);
+            this.dragons.splice(index, 1);
+        },
+        addMonster() {
+            this.monsters.push({
+                name: this.newMonster.name,
+                quickness: parseInt(this.newMonster.quickness),
+                initiative: 0,
+            });
+            this.newMonster.name = null;
+            this.newMonster.quickness = null;
+        },
+        deleteMonster(monster) {
+            let index = this.monsters.findIndex(s => s === monster);
+            this.monsters.splice(index, 1);
+        },
+        calculateTurns() {
+            this.turns = [];
+
+            for (let dragon of this.dragons) {
+                if (dragon.ambush > 0) this.turns.push(dragon.name);
+                if (dragon.ambush > 1) this.turns.push(dragon.name);
+            }
+
+            if (this.monsters.length === 0) return;
+
+            let turncost = this.monsters[this.monsters.length - 1].quickness;
+            console.log("Turn cost: " + turncost);
+
+            let combatants = [];
+            this.dragons.forEach(d => {
+                d.initiative = 0;
+                combatants.push(d);
+            });
+            this.monsters.forEach(m => {
+                m.initiative = 0;
+                combatants.push(m);
+            });
+
+            for (let i = 0; i < this.numRounds; i++) {
+                combatants.forEach(c => c.initiative += c.quickness);
+
+                let minBreath = turncost;
+                while (minBreath >= turncost) {
+                    minBreath = 0;
+                    combatants.sort((a,b) => b.initiative - a.initiative);
+                    for (let c of combatants) {
+                        if (c.initiative >= turncost) {
+                            this.turns.push(c.name);
+                            c.initiative -= turncost;
+                            console.log(c.initiative);
+                        }
+                        minBreath = Math.max(minBreath, c.initiative);
+                    }
+                }
+            }
+
+            this.turnsCalculated = true;
+        }
+    }
+});
